0babd9cf309fa48774a2a51258d1f16c37613e71
hendratrias <htrias4@gmail.com>
2021-01-15 10:52:07 +0700

    menambahkan fitur pencegahan

--- lib/info_screen.dart
+++ lib/info_screen.dart
@@ -38,16 +38,40 @@ class _InfoScreenState extends State<InfoScreen> { // widget controller
       body: SingleChildScrollView(
         controller: controller,
 
-        child: Column( //menata widget column
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            MyHeader(
-              image: "assets/icons/coronadr.svg",
-              textTop: "Cari Tahu ",
-              textBottom: "Tentang Covid-19.",
-              offset: offset,
-            ),
-            
+
+                  SizedBox(height: 20), //ukuran box
+                  Text("Pencegahan", style: kTitleTextstyle),
+                  SizedBox(height: 20),
+                  PreventCard(
+                    text:
+                    "Gunakan masker yang sesuai standart kesehatan saat keluar rumah untuk mencegah masuknya virus secara langsung",
+                    image: "assets/images/mask_1.png",
+                    title: "Menggunakan Masker",
+                  ),
+                  PreventCard(
+                    text:
+                    " Biasakan mencuci tangan setelah keluar rumah menggunakan sabun atau handsanitizer",
+                    image: "assets/images/wash_your_hands_1.png",
+                    title: "Cuci Tangan",
+                  ),
+                  PreventCard(
+                    text:
+                    "Jaga jarak aman dengan orang lain sekitar 1-2 meter dan hindari kontak sosial serta hindari kerumunan",
+                    image: "assets/images/social_distancing_1.png",
+                    title: "Jaga Jarak",
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            )
+          ],
+
+        ),
+
+      ),
+    );
+  }
+}
 
 class PreventCard extends StatelessWidget { // widget yang dapat diperbarui sesuai kebutuhan
   final String image;
